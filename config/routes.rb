4e8ed8cb73a6357ba89b6413d1de1ec77f6bba63
Lucas Gruner <lucas.gruner@gmail.com>
2025-08-15 07:04:18 +0200

    Add a list, add a movie to a list (new and create for lists and bookmarks)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show]
+  root "lists#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
